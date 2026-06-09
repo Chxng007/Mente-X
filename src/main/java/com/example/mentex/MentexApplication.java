31d1a02c0b589d45f048c7a8e6e655097693a58a
Chxng007 <jchangmoreno5@gmail.com>
2026-06-09 16:00:10 -0500

    creando repo

---
+++ src/main/java/com/example/mentex/MentexApplication.java
@@ -0,0 +1,13 @@
+package com.example.mentex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mentex/MentexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mentex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
